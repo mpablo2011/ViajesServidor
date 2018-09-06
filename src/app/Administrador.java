0074ded8598a03992c16ed3341f4d0138eb4244a
mpablo2011 <32689365+mpablo2011@users.noreply.github.com>
2018-09-06 11:36:53 -0300

    server

--- src/app/Administrador.java
+++ src/app/Administrador.java
@@ -1,5 +1,126 @@
 package app;
 
-public class Administrador {
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.List;
+import java.util.Vector;
+
+import bean.Cliente;
+import bean.Views.ClienteView;
+import bean.Views.PaqueteView;
+import bean.Views.ProductoView;
+import bean.Views.VentaView;
+import interfaz.TDAManejoDatos;
+
+public class Administrador extends UnicastRemoteObject implements TDAManejoDatos{
+
+	public Administrador() throws RemoteException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Cliente buscarCliente(int dni) {
+		// TODO Auto-generated method stub
+		return ClienteController.getInstancia().buscarCliente(dni);
+	}
+
+	@Override
+	public int altaCliente(int dni, String nombre, int telefono, String mail) {
+		// TODO Auto-generated method stub
+		return ClienteController.getInstancia().altaCliente(dni, nombre, telefono, mail);
+	}
+
+	@Override
+	public int bajaCliente(int dni) {
+		// TODO Auto-generated method stub
+		return ClienteController.getInstancia().bajaCliente(dni);
+	}
+
+	@Override
+	public ProductoView getProductoPorCodigo(int codigo) {
+		// TODO Auto-generated method stub
+		return ProductoController.getInstancia().getProductoPorCodigo(codigo);
+	}
+
+	@Override
+	public void grabarAlojamiento(String descripcion, String fechaDesde, String fechaHasta, String nombre, float precio,
+			String ubicacion) {
+		ProductoController.getInstancia().grabarAlojamiento(descripcion, fechaDesde, fechaHasta, nombre, precio, ubicacion);
+		
+	}
+
+	@Override
+	public void grabarPasaje(String descripcion, String fecha, String aerolinea, String origen, String destino,
+			float precio) {
+		ProductoController.getInstancia().grabarPasaje(descripcion, fecha, aerolinea, origen, destino, precio);
+		
+	}
+
+	@Override
+	public void grabarVisita(String descripcion, String fecha, String nombre, String ubicacion, float precio) {
+		// TODO Auto-generated method stub
+		ProductoController.getInstancia().grabarVisita(descripcion, fecha, nombre, ubicacion, precio);
+	}
+
+	@Override
+	public void grabarPaquete(PaqueteView paqueteVw) {
+		// TODO Auto-generated method stub
+		ProductoController.getInstancia().grabarPaquete(paqueteVw);
+	}
+
+	@Override
+	public void cargaInicial() {
+		// TODO Auto-generated method stub
+		MainSistemaDeVentas.getInstancia().cargaInicial();
+	}
+
+	@Override
+	public List<Cliente> getClientes() {
+		// TODO Auto-generated method stub
+		return MainSistemaDeVentas.getInstancia().getClientes();
+	}
+
+	@Override
+	public void addCliente(Cliente cliente) {
+		// TODO Auto-generated method stub
+		MainSistemaDeVentas.getInstancia().addCliente(cliente);
+	}
+
+	@Override
+	public int VincularClienteAVenta(int dni) {
+		// TODO Auto-generated method stub
+		return VentaController.getInstancia().VincularClienteAVenta(dni);
+	}
+
+	@Override
+	public void agregarProducto(int codproducto, int cant) {
+		// TODO Auto-generated method stub
+		VentaController.getInstancia().agregarProducto(codproducto, cant);
+	}
+
+	@Override
+	public VentaView getVentaView() {
+		// TODO Auto-generated method stub
+		return VentaController.getInstancia().getVentaView();
+	}
+
+	@Override
+	public ClienteView getClienteView() {
+		// TODO Auto-generated method stub
+		return VentaController.getInstancia().getClienteView();
+	}
+
+	@Override
+	public Vector getItemVentaVector() {
+		// TODO Auto-generated method stub
+		return VentaController.getInstancia().getItemVentaVector();
+	}
+
+	@Override
+	public void grabarVenta() {
+		// TODO Auto-generated method stub
+		VentaController.getInstancia().grabarVenta();
+	}
 
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -1,130 +1,28 @@
 package server;
 
+import java.rmi.Naming;
 import java.rmi.RemoteException;
-import java.rmi.server.UnicastRemoteObject;
-import java.util.List;
-import java.util.Vector;
+import java.rmi.registry.LocateRegistry;
 
-import app.ClienteController;
-import app.MainSistemaDeVentas;
-import app.ProductoController;
-import app.VentaController;
-import bean.Cliente;
-import bean.Views.ClienteView;
-import bean.Views.PaqueteView;
-import bean.Views.ProductoView;
-import bean.Views.VentaView;
-import interfaz.TDAManejoDatos;
-
-public class Server extends UnicastRemoteObject implements TDAManejoDatos{
-
-	protected Server() throws RemoteException {
-		super();
-		// TODO Auto-generated constructor stub
+import app.Administrador;
+public class Server{
+	
+	Administrador objetoRemoto;
+	
+	public Server() {
+		iniciar();
 	}
+	
+    public void iniciar() {
+    	try {
+    		LocateRegistry.createRegistry(1099);	
+            this.objetoRemoto = new Administrador();
+            Naming.rebind ("//localhost/GestionViajes", objetoRemoto);
+            System.out.println("Fijado en //localhost/GestionViajes");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
 
-	@Override
-	public Cliente buscarCliente(int dni) {
-		// TODO Auto-generated method stub
-		return ClienteController.getInstancia().buscarCliente(dni);
-	}
-
-	@Override
-	public int altaCliente(int dni, String nombre, int telefono, String mail) {
-		// TODO Auto-generated method stub
-		return ClienteController.getInstancia().altaCliente(dni, nombre, telefono, mail);
-	}
-
-	@Override
-	public int bajaCliente(int dni) {
-		// TODO Auto-generated method stub
-		return ClienteController.getInstancia().bajaCliente(dni);
-	}
-
-	@Override
-	public ProductoView getProductoPorCodigo(int codigo) {
-		// TODO Auto-generated method stub
-		return ProductoController.getInstancia().getProductoPorCodigo(codigo);
-	}
-
-	@Override
-	public void grabarAlojamiento(String descripcion, String fechaDesde, String fechaHasta, String nombre, float precio,
-			String ubicacion) {
-		ProductoController.getInstancia().grabarAlojamiento(descripcion, fechaDesde, fechaHasta, nombre, precio, ubicacion);
-		
-	}
-
-	@Override
-	public void grabarPasaje(String descripcion, String fecha, String aerolinea, String origen, String destino,
-			float precio) {
-		ProductoController.getInstancia().grabarPasaje(descripcion, fecha, aerolinea, origen, destino, precio);
-		
-	}
-
-	@Override
-	public void grabarVisita(String descripcion, String fecha, String nombre, String ubicacion, float precio) {
-		// TODO Auto-generated method stub
-		ProductoController.getInstancia().grabarVisita(descripcion, fecha, nombre, ubicacion, precio);
-	}
-
-	@Override
-	public void grabarPaquete(PaqueteView paqueteVw) {
-		// TODO Auto-generated method stub
-		ProductoController.getInstancia().grabarPaquete(paqueteVw);
-	}
-
-	@Override
-	public void cargaInicial() {
-		// TODO Auto-generated method stub
-		MainSistemaDeVentas.getInstancia().cargaInicial();
-	}
-
-	@Override
-	public List<Cliente> getClientes() {
-		// TODO Auto-generated method stub
-		return MainSistemaDeVentas.getInstancia().getClientes();
-	}
-
-	@Override
-	public void addCliente(Cliente cliente) {
-		// TODO Auto-generated method stub
-		MainSistemaDeVentas.getInstancia().addCliente(cliente);
-	}
-
-	@Override
-	public int VincularClienteAVenta(int dni) {
-		// TODO Auto-generated method stub
-		return VentaController.getInstancia().VincularClienteAVenta(dni);
-	}
-
-	@Override
-	public void agregarProducto(int codproducto, int cant) {
-		// TODO Auto-generated method stub
-		VentaController.getInstancia().agregarProducto(codproducto, cant);
-	}
-
-	@Override
-	public VentaView getVentaView() {
-		// TODO Auto-generated method stub
-		return VentaController.getInstancia().getVentaView();
-	}
-
-	@Override
-	public ClienteView getClienteView() {
-		// TODO Auto-generated method stub
-		return VentaController.getInstancia().getClienteView();
-	}
-
-	@Override
-	public Vector getItemVentaVector() {
-		// TODO Auto-generated method stub
-		return VentaController.getInstancia().getItemVentaVector();
-	}
-
-	@Override
-	public void grabarVenta() {
-		// TODO Auto-generated method stub
-		VentaController.getInstancia().grabarVenta();
-	}
 
 }
